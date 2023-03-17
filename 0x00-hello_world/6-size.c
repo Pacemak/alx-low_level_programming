1f8e053d5d8f0220049346c2056d2fb1d069bb71
Blossom Osemelomen Osobase <blossompaul52@gmail.com>
2023-03-17 00:15:23 -0700

    size c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %d byte(s)", sizeof(char));
-prinf("size of an int: %d byte(s)", sizeof(int));
-printf("size of long int: %d byte(s)", sizeof(long int));
-printf("size of long long int: %d byte(s)", sizeof(long long int));
-printf("size of float %d byte(s)", sizeof(float));
-return (0);
+	printf("Size of a char: %d byte(s)", sizeof(char));
+	prinf("size of an int: %d byte(s)", sizeof(int));
+	printf("size of long int: %d byte(s)", sizeof(long int));
+	printf("size of long long int: %d byte(s)", sizeof(long long int));
+	printf("size of float %d byte(s)", sizeof(float));
+	return (0);
 }
