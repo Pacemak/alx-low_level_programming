4643bc378dc14872c707def54791db9c0b8e7b33
Blossom Osemelomen Osobase <blossompaul52@gmail.com>
2023-03-17 00:02:56 -0700

    size c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the sixe of various computer types
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %d byte(s)", sizeof(char));
+prinf("size of an int: %d byte(s)", sizeof(int));
+printf("size of long int: %d byte(s)", sizeof(long int));
+printf("size of long long int: %d byte(s)", sizeof(long long int));
+printf("size of float %d byte(s)", sizeof(float));
+return (0);
+}
