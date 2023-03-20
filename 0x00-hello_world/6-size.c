f38b4967eb29347b2a69699799e8e7d85626fce1
Blossom Osemelomen Osobase <blossompaul52@gmail.com>
2023-03-20 06:34:50 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,15 +2,15 @@
 /**
  * main - A program that prints the sixe of various computer types
  *
- * Return: 0 (Success)
+ * Return: always 0
  */
 
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	prinf("size of an int: %lu byte(s)\n", sizeof(int));
-	printf("size of long int: %lu byte(s)\n", sizeof(long int));
-	printf("size of long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("size of float: %lu byte(s)\n", sizeof(float));
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	prinf("size of an int: %i byte(s)\n", sizeof(int));
+	printf("size of long int: %i byte(s)\n", sizeof(long int));
+	printf("size of long long int: %i byte(s)\n", sizeof(long long int));
+	printf("size of float: %i byte(s)\n", sizeof(float));
 	return (0);
 }
