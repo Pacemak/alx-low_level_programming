f87d1799225ef3d4503cb2dd8c8d9dc7aa6cc4a5
Blossom Osemelomen Osobase <blossompaul52@gmail.com>
2023-03-20 05:02:50 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,12 +4,13 @@
  *
  * Return: 0 (Success)
  */
+
 int main(void)
 {
-printf("Size of a char: %d byte(s)\n", sizeof(char));
-prinf("size of an int: %d byte(s)\n", sizeof(int));
-printf("size of long int: %d byte(s)\n", sizeof(long int));
-printf("size of long long int: %d byte(s)\n", sizeof(long long int));
-printf("size of float %d byte(s)\n", sizeof(float));
-return (0);
+	printf("Size of a char: %lu byte(s)", sizeof(char));
+	prinf("size of an int: %lu byte(s)", sizeof(int));
+	printf("size of long int: %lu byte(s)", sizeof(long int));
+	printf("size of long long int: %lu byte(s)", sizeof(long long int));
+	printf("size of float: %lu byte(s)", sizeof(float));
+	return (0);
 }
