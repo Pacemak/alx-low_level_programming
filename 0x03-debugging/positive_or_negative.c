30d23de83d7bf15820d1c2709391a2460633efd3
Blossom Osemelomen Osobase <blossompaul52@gmail.com>
2023-03-23 03:17:30 -0700

    0 Task

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * positive_or_negative - checks for positve or nagative numbers
+ *
+ * @i: parameter function
+ *
+ * Return: always 0
+ */
+
+void positive_or_negative(int i)
+{
+	if (1 < 0)
+		printf("%d is nagative\n", i);
+	else
+		printf("%d is positive\n", i);
+}
