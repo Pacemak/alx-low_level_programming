1f6b584aadfe25b7ee69f4a6eca3858a96341fe0
Blossom Osemelomen Osobase <blossompaul52@gmail.com>
2023-03-30 17:55:23 -0700

    7. Mozart composed his music

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,15 +1,13 @@
 #include "main.h"
-
 /**
- * leet -  function that encodes a string into 1337speak
- * @n: input
- * Return: n
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
  */
 char *leet(char *n)
 {
-	int i;
-	int j;
-	char s1[] = "aAeE[o0tTIL";
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
 	for (i = 0; n[i] != '\0'; i++)
